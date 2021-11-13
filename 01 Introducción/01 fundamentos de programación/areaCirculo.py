f4349b8444c36df2e62d3192479fb3af573ec47f
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2021-11-13 12:06:19 -0300

    Create areaCirculo.py

---
+++ 01 Introducción/01 fundamentos de programación/areaCirculo.py
@@ -0,0 +1,7 @@
+#areaCirculo: num -> float
+#calcula area de circulo de radio r
+#ejemplo: areaCirculo(1) debe entregar 3.14
+def areaCirculo(r):
+    pi=3.14
+    return pi*r**2
+assert areaCirculo(1)==3.14
